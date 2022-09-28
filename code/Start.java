240a9e1b7f2e1a6b057ec7134cf97f176bb4c5fe
Sharinogan <voragun2543@gmail.com>
2022-09-28 17:20:00 +0700

    read file

--- code/Start.java
+++ code/Start.java
@@ -1,24 +1,19 @@
-import java.util.Date;
+import java.io.FileReader;
 
 class Start {
     public static void main(String[] cookies) {
-        Object[] list = { 8, 5, 2.5, 4, 4.0, 7.2 };
-        // Write code to count number of instance of Integer
-        int count = 0;
-        for (int i = 0; i < list.length; i++) {
-            if (list[i] instanceof Integer) {
-                count++;
+        try {
+            FileReader f = new FileReader("data.txt");
+            while(true) {
+                    int k = f.read();
+                    if (k < 0) {
+                        break;
+                    }
+                System.out.print((char)k);
             }
+        } catch (Exception e) {
+            System.out.println("File not found");
         }
-        System.out.println("Count : " + count);
-        
-        Object[] a = { "Latte", 1.5, 8, new Date(), "Mocha" };
-        
-        System.out.println(a[0] instanceof String);
-        System.out.println(a[1] instanceof Double);
-        System.out.println(a[2] instanceof Integer);
-        
-        System.out.println("Hello");
     }
 }
 
