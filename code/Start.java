846ccd635e7241df9c9631b71efc2c8b04f68cc0
Sharinogan <voragun2543@gmail.com>
2022-09-28 16:17:14 +0700

    Example final class

---
+++ code/Start.java
@@ -0,0 +1,9 @@
+class Start {
+    public static void main(String[] cookies) {
+        System.out.println("Hello");
+    }
+}
+
+class Employee { }
+final class Contractor extends Employee { }
+// class  Daily extends Contractor { }  // ERROR
